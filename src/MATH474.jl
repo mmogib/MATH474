234d573a6fffcf4c3853b79e184fb14cdb13f7fb
Mohammed Alshahrani <mmogib@gmail.com>
2024-01-17 15:50:41 +0300

    initial commit

--- src/MATH474.jl
+++ src/MATH474.jl
@@ -2,4 +2,14 @@ module MATH474
 
 # Write your package code here.
 
+function addit(x, y)
+    x + y
+end
+
+function addone(x)
+    addit(1, x)
+end
+
+export addit, addone
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "MATH474.jl" begin
     # Write your tests here.
+    @test addone(5) == 6
 end
