d705f2233febc47fbe7a6518888143c7accacd86
Documenter.jl <documenter@juliadocs.github.io>
2024-01-17 12:52:42 +0000

    build based on 234d573

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MATH474","category":"page"},{"location":"#MATH474","page":"Home","title":"MATH474","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MATH474.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MATH474]","category":"page"}]
+}
